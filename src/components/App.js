6efc98691764824d88b028e2ea150e07e924d6f2
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 13:55:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,16 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import MarkdownEditor from "../components/MarkdownEditor";
+import "../styles.css";
+
+const App = () => {
+  return (
+    <div id="main">
+      <h1>React Markdown Editor</h1>
+      <MarkdownEditor />
+    </div>
+  );
+};
+
+export default App;
+
 
