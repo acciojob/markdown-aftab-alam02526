5fdec8509cdfbcba8e449b72c9039b6aa78daffd
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 14:11:07 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-let markedLib;
-(async () => {
-  const { marked } = await import("marked");
-  markedLib = marked;
-})();
-
 const MarkdownEditor = () => {
   const [markdown, setMarkdown] = useState("");
   const [preview, setPreview] = useState("");
 
   useEffect(() => {
-    if (markedLib) {
-      setPreview(markedLib(markdown));
+    if (window.marked) {
+      setPreview(window.marked.parse(markdown));
     }
   }, [markdown]);
 
@@ -24,20 +18,19 @@ const MarkdownEditor = () => {
           id="editor"
           value={markdown}
           onChange={(e) => setMarkdown(e.target.value)}
-          placeholder="Type your markdown here..."
-        ></textarea>
+        />
       </div>
-
       <div className="markdown-preview">
         <h2>Preview</h2>
         <div
           id="preview"
           dangerouslySetInnerHTML={{ __html: preview }}
-        ></div>
+        />
       </div>
     </div>
   );
 };
 
 export default MarkdownEditor;
+
 
